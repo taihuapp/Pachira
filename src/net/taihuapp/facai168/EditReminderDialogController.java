1b891218ec67e422619287bd56a888eb5e707437
Guangliang He <Guangliang.He@gmail.com>
2016-12-07 14:06:11 -0500

    Got edit reminder ui working now.

--- src/net/taihuapp/facai168/EditReminderDialogController.java
+++ src/net/taihuapp/facai168/EditReminderDialogController.java
@@ -4,6 +4,8 @@ import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
+import javafx.util.converter.BigDecimalStringConverter;
 import javafx.util.converter.NumberStringConverter;
 
 import java.util.concurrent.Callable;
@@ -13,10 +15,57 @@ import java.util.concurrent.Callable;
  *
  */
 public class EditReminderDialogController {
+
+    private class CategoryIDConverter extends StringConverter<Integer> {
+        public Integer fromString(String categoryName) {
+            Category c = mMainApp.getCategoryByName(categoryName);
+            return c == null ? 0 : c.getID();
+        }
+        public String toString(Integer cid) {
+            Category c = mMainApp.getCategoryByID(cid);
+            return c == null ? "" : c.getName();
+        }
+    }
+
+    // this converts account id to account name
+    // different from the converter in EditTransactions
+    private class AccountIDConverter extends StringConverter<Integer> {
+        public Integer fromString(String accountName) {
+            Account a = mMainApp.getAccountByName(accountName);
+            return a == null ? 0 : a.getID();
+        }
+        public String toString(Integer aid) {
+            Account a = mMainApp.getAccountByID(aid);
+            return a == null ? "" : a.getName();
+        }
+    }
+
     private MainApp mMainApp;
     private Reminder mReminder;
     private Stage mDialogStage;
 
+    @FXML
+    private ChoiceBox<Reminder.Type> mTypeChoiceBox;
+    @FXML
+    private TextField mPayeeTextField;
+    @FXML
+    private TextField mAmountTextField;
+    @FXML
+    private Label mAccountIDLabel;
+    @FXML
+    private ComboBox<Integer> mAccountIDComboBox;
+    @FXML
+    private Label mCategoryIDLabel;
+    @FXML
+    private ComboBox<Integer> mCategoryIDComboBox;
+    @FXML
+    private Label mTransferAccountIDLabel;
+    @FXML
+    private ComboBox<Integer> mTransferAccountIDComboBox;
+    @FXML
+    private TextField mTagTextField;
+    @FXML
+    private TextField mMemoTextField;
     @FXML
     private ChoiceBox<DateSchedule.BaseUnit> mBaseUnitChoiceBox;
     @FXML
@@ -49,6 +98,19 @@ public class EditReminderDialogController {
         mFWDToggleButton.setToggleGroup(mFWDGroup);
         mREVToggleButton.setToggleGroup(mFWDGroup);
         // todo need a changelistenser for mCountBeforeEndTextField
+
+        mTypeChoiceBox.getItems().setAll(Reminder.Type.values());
+        final Callable<Boolean> converter0 = () -> mTypeChoiceBox.valueProperty().get() != Reminder.Type.TRANSFER;
+        mCategoryIDLabel.visibleProperty().bind(Bindings.createBooleanBinding(converter0,
+                mTypeChoiceBox.valueProperty()));
+        mCategoryIDComboBox.visibleProperty().bind(Bindings.createBooleanBinding(converter0,
+                mTypeChoiceBox.valueProperty()));
+
+        final Callable<Boolean> converter1 = () ->  mTypeChoiceBox.valueProperty().get() == Reminder.Type.TRANSFER;
+        mTransferAccountIDLabel.visibleProperty().bind(Bindings.createBooleanBinding(converter1,
+                mTypeChoiceBox.valueProperty()));
+        mTransferAccountIDComboBox.visibleProperty().bind(Bindings.createBooleanBinding(converter1,
+                mTypeChoiceBox.valueProperty()));
     }
 
     void setMainApp(MainApp mainApp, Reminder reminder, Stage stage) {
@@ -57,6 +119,40 @@ public class EditReminderDialogController {
         mDialogStage = stage;
 
         // bind the properties now
+        // seems no need to do unbindbidirectional
+        mTypeChoiceBox.valueProperty().bindBidirectional(mReminder.getTypeProperty());
+        mPayeeTextField.textProperty().bindBidirectional(mReminder.getPayeeProperty());
+        mAmountTextField.textProperty().bindBidirectional(mReminder.getAmountProperty(),
+                new BigDecimalStringConverter());
+
+        mAccountIDComboBox.setConverter(new AccountIDConverter());
+        mAccountIDComboBox.getItems().clear();
+        for (Account a : mMainApp.getAccountList(Account.Type.SPENDING, false, true))
+            mAccountIDComboBox.getItems().add(a.getID());
+        Bindings.bindBidirectional(mAccountIDComboBox.valueProperty(), mReminder.getAccountIDProperty().asObject());
+
+        mCategoryIDComboBox.setConverter(new CategoryIDConverter());
+        mCategoryIDComboBox.getItems().clear();
+        for (Category c : mMainApp.getCategoryList())
+            mCategoryIDComboBox.getItems().add(c.getID());
+        Bindings.bindBidirectional(mCategoryIDComboBox.valueProperty(), mReminder.getCategoryIDProperty().asObject());
+
+        mTransferAccountIDComboBox.setConverter(new AccountIDConverter());
+        mTransferAccountIDComboBox.getItems().clear();
+        for (Account a : mMainApp.getAccountList(Account.Type.SPENDING, false, true))
+            mAccountIDComboBox.getItems().add(a.getID());
+        Bindings.bindBidirectional(mTransferAccountIDComboBox.valueProperty(),
+                mReminder.getTransferAccountIDProperty().asObject());
+
+        mTransferAccountIDComboBox.setConverter(new AccountIDConverter());
+        mTransferAccountIDComboBox.getItems().clear();
+        for (Account a : mMainApp.getAccountList(Account.Type.SPENDING, false, true))
+            mTransferAccountIDComboBox.getItems().add(a.getID());
+
+        mTagTextField.textProperty().bindBidirectional(mReminder.getTagProperty());
+        mMemoTextField.textProperty().bindBidirectional(mReminder.getMemoProperty());
+
+        // bind properties for DateSchedule fields
         mBaseUnitChoiceBox.valueProperty().bindBidirectional(mReminder.getDateSchedule().getBaseUnitProperty());
         mStartDatePicker.valueProperty().bindBidirectional(mReminder.getDateSchedule().getStartDateProperty());
         mEndDatePicker.valueProperty().bindBidirectional(mReminder.getDateSchedule().getEndDateProperty());
@@ -95,6 +191,7 @@ public class EditReminderDialogController {
                 mReminder.getDateSchedule().getBaseUnitProperty()));
 
         mDSDescriptionLabel.textProperty().bind(mReminder.getDateSchedule().getDescriptionProperty());
+
     }
 
     @FXML
--- src/net/taihuapp/facai168/Reminder.java
+++ src/net/taihuapp/facai168/Reminder.java
@@ -1,7 +1,9 @@
 package net.taihuapp.facai168;
 
+import javafx.beans.property.*;
 import javafx.util.converter.DateStringConverter;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 /**
@@ -9,14 +11,60 @@ import java.time.LocalDate;
  *
  */
 class Reminder {
+
+    enum Type { PAYMENT, DEPOSIT, TRANSFER }
+
+    private int mID = -1;
+    private final ObjectProperty<Type> mTypeProperty = new SimpleObjectProperty<>(Type.PAYMENT);
+    private final StringProperty mPayeeProperty = new SimpleStringProperty("");
+    private final ObjectProperty<BigDecimal> mAmountProperty = new SimpleObjectProperty<>(BigDecimal.ZERO);
+    private final IntegerProperty mAccountIDProperty = new SimpleIntegerProperty(0);
+    private final IntegerProperty mCategoryIDProperty = new SimpleIntegerProperty(0);
+    private final IntegerProperty mTransferAccountIDProperty = new SimpleIntegerProperty(0);
+    private final StringProperty mTagProperty = new SimpleStringProperty("");
+    private final StringProperty mMemoProperty = new SimpleStringProperty("");
+
     private DateSchedule mDateSchedule;
 
     // default constructor
     Reminder() {
+
         // default monthly schedule, starting today, no end, counting day of month forward.
         mDateSchedule = new DateSchedule(DateSchedule.BaseUnit.MONTH, 1, LocalDate.now(), null,
                 true, true);
     }
 
     DateSchedule getDateSchedule() { return mDateSchedule; }
+
+    ObjectProperty<Type> getTypeProperty() { return mTypeProperty; }
+    Type getType() { return getTypeProperty().get(); }
+    void setTyp(Type t) { getTypeProperty().set(t); }
+
+    StringProperty getPayeeProperty() { return mPayeeProperty; }
+    String getPayee() { return getPayeeProperty().get(); }
+    void setPayee(String p) { getPayeeProperty().set(p); }
+
+    ObjectProperty<BigDecimal> getAmountProperty() { return mAmountProperty; }
+    BigDecimal getAmount() { return getAmountProperty().get(); }
+    void setAmount(BigDecimal a) { getAmountProperty().set(a); }
+
+    IntegerProperty getAccountIDProperty() { return mAccountIDProperty; }
+    Integer getAccountID() { return getAccountIDProperty().get(); }
+    void setAccountID(int i) { getAccountIDProperty().set(i); }
+
+    IntegerProperty getCategoryIDProperty() { return mCategoryIDProperty; }
+    Integer getCategoryID() { return getCategoryIDProperty().get(); }
+    void setCategoryID(int i) { getCategoryIDProperty().set(i); }
+
+    IntegerProperty getTransferAccountIDProperty() { return mTransferAccountIDProperty; }
+    Integer getTransferAccountID() { return getTransferAccountIDProperty().get(); }
+    void setTransferAccountID(int i) { getTransferAccountIDProperty().set(i); }
+
+    StringProperty getTagProperty() { return mTagProperty; }
+    String getTag() { return getTagProperty().get(); }
+    void setTag(String t) { getTagProperty().set(t); }
+
+    StringProperty getMemoProperty() { return mMemoProperty; }
+    String getMemo() { return getMemoProperty().get(); }
+    void setMemo(String m) { getMemoProperty().set(m); }
 }

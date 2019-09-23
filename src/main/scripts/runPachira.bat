9363e3fa3427e29c5f1d0c98d2a63046e9f64033
Guangliang He <Guangliang.He@gmail.com>
2019-09-23 04:42:31 -0400

    v0.3.0.0 fix batch script

--- src/main/scripts/runPachira.bat
+++ src/main/scripts/runPachira.bat
@@ -1,7 +1,8 @@
 @echo off
 java -version 1>nul 2>nul || (
-   echo no java installed
-   exit /b 1
+  echo no java installed
+  pause
+  exit /b 1
 )
 
 REM put an extra ^" just so emacs can be happy
@@ -10,14 +11,17 @@ for /f tokens^=2^ delims^=^"^"^. %%m in ('java -version 2^>^&1 ^|^ findstr versi
 if %major% LSS 11 (
   echo java version is too low 
   echo at least 11 is needed
+  pause
   exit /b 1
 )
 
-if exist %~dp0%Pachira.jar (
-  if not defined PATH_TO_FX (
-    set "PATH_TO_FX=C:\Program Files\Java\javafx-sdk-11.0.2\lib"
-  )
-  java --module-path="%PATH_TO_FX%" --add-modules=javafx.controls,javafx.fxml -jar %~dp0%Pachira.jar
-) else (
-  echo Cannot find Pachira.jar
+if not exist %~dp0%Pachira.jar (
+  echo Pachira.jar not found
+  pause
+  exit /b 1
 )
+
+if not defined PATH_TO_FX (
+  set "PATH_TO_FX=C:\Program Files\Java\javafx-sdk-11.0.2\lib"
+)
+java --module-path="%PATH_TO_FX%" --add-modules javafx.controls,javafx.fxml -jar %~dp0%Pachira.jar

49ff4280c6f9e2174f55344cbed07f2c862845dd
taihuapp <taihuapp@gmail.com>
2023-04-27 10:54:20 -0400

    added account name validation in EditAccountDialogController

--- src/main/java/net/taihuapp/pachira/EditAccountDialogController.java
+++ src/main/java/net/taihuapp/pachira/EditAccountDialogController.java
@@ -75,10 +75,10 @@ public class EditAccountDialogController {
 
     @FXML
     private void handleOK() {
-        String name = mNameTextField.getText();
+        final String name = mNameTextField.getText();
         if (name == null || name.length() == 0 || MainApp.hasBannedCharacter(name)) {
             // we need to throw up a warning sign and go back
-            Alert alert = new Alert(Alert.AlertType.WARNING);
+            final Alert alert = new Alert(Alert.AlertType.WARNING);
             alert.setTitle("Warning");
             if (name == null || name.length() == 0)
                 alert.setHeaderText("Account name cannot be empty");
@@ -87,6 +87,16 @@ public class EditAccountDialogController {
                 bcs = bcs.substring(1, bcs.length() - 1).replaceAll(",", ""); // take out [] and ,
                 alert.setHeaderText("Account name cannot contain any of:  " + bcs);
             }
+            alert.showAndWait();
+            return;
+        }
+
+        if (((account == null) || (name.compareTo(account.getName()) != 0))
+                && mainModel.getAccount(a -> a.getName().equals(name)).isPresent()) {
+            // name is used by another account already
+            final Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText("Name '" + name + "' is already used.");
             alert.showAndWait();
             return;
         }
